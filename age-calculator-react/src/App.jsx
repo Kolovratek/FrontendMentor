0cfe9b59a4fc492e2d80156e9f3f95384fb04947
Milan Pankuch <milan.pankuch@student.tuke.sk>
2023-05-16 16:01:40 +0200

    Create react app

---
+++ age-calculator-react/src/App.jsx
@@ -0,0 +1,15 @@
+import Inputs from "./components/Inputs";
+import MainData from "./components/MainData";
+
+function App() {
+  return (
+    <div className="card">
+      <div className="card-div">
+        <Inputs />
+        <MainData />
+      </div>
+    </div>
+  );
+}
+
+export default App;
